e43c6742f695a345acd4710303bd84eaf19a9429
kons16 <ks1216r@gmail.com>
2018-11-18 19:36:43 +0900

    Archiveの修正

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,35 +2,35 @@ class MicropostsController < ApplicationController
 	before_action :logged_in_user, only:[:new, :create, :destroy]
  	before_action :correct_user, only: :destroy
 
-  def new
-    @micropost = Micropost.new
-  end
+	def new
+		@micropost = Micropost.new
+	end
 
-  def create
- 	@micropost = current_user.microposts.build(micropost_params)
+	def create
+		@micropost = current_user.microposts.build(micropost_params)
 
-  	if @micropost.save
-  		redirect_to current_user
-  	else
-  		render 'new'
-  	end
-  end
+		if @micropost.save
+			redirect_to current_user
+		else
+			render 'new'
+		end
+	end
 
-  def destroy
-    @micropost.destroy
-    redirect_to request.referrer || root_url
-  end
+	def destroy
+		@micropost.destroy
+		redirect_to request.referrer || root_url
+	end
 
-  private
+	private
 
-  	def micropost_params
-  		params.require(:micropost).permit(:year, :month, :day, :teama, :teamb,
-  		 	:place, :myteam, :artist, :livetitle, :logo, :apoint, :bpoint)
-  	end
+		def micropost_params
+			params.require(:micropost).permit(:year, :month, :day, :teama, :teamb,
+				:place, :myteam, :artist, :livetitle, :logo, :apoint, :bpoint)
+		end
 
-  	def correct_user
-  		@micropost = current_user.microposts.find_by(id: params[:id])
-  		redirect_to root_url if @micropost.nil?
-  	end
+		def correct_user
+			@micropost = current_user.microposts.find_by(id: params[:id])
+			redirect_to root_url if @micropost.nil?
+		end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,19 @@ class UsersController < ApplicationController
 	before_action :logged_in_user, only: [:index, :edit, :update]
 	before_action :correct_user,   only: [:edit, :update, :add]
 
-
+	# マイページにアクセス
 	def show
     	@user = User.find(params[:id])
     	@microposts =  @user.microposts.paginate(:page => params[:page], :per_page => 5)
+    	# 並び替え
+    	@microposts = @microposts.order(year: "DESC")
+    	@microposts = @microposts.order(month: "DESC")
+    	@microposts = @microposts.order(day: "DESC")
+
+    	
 	end
 
 	def index
-
 	end
 
 	private
