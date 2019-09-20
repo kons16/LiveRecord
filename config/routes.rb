4ec0d69ed2f68c4b4311e381983a6b3ca592f2d6
kons16 <ks1216r@gmail.com>
2019-09-20 21:35:10 +0900

    リファクタリング

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
 
-	devise_for :users
-	resources :users
-    resources :microposts
+  devise_for :users
+  resources :users
+  resources :microposts
 
-	root 'pages#index'
-	resources :users do
-		member do
-			get :following, :followers
-		end
-	end
+  root 'pages#index'
+  resources :users do
+    member do
+     get :following, :followers
+   end
+ end
 
-	resources :relationships,	only: [:create, :destroy]
+ resources :relationships,	only: [:create, :destroy]
 end
